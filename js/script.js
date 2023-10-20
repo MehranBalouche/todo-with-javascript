efabc4e0c4b0db74ae31ceb3a9d39c5999dc912a
mehran <mehransheyhki11@yahoo.com>
2023-10-20 09:35:08 +0330

    removed temp file

--- js/script.js
+++ js/script.js
@@ -32,7 +32,6 @@ let $ = document;
     function syncToStorage(myTodos){
         localStorage.setItem('m-todosData', JSON.stringify(myTodos))
     }
-    
     function getTodos(){
 
         localStorageTodos = JSON.parse(localStorage.getItem('m-todosData'))
@@ -59,8 +58,9 @@ let $ = document;
                             ${ todo.completed ? `<input data-id="${todo.id}" onclick="changeStatus(event)" class="form-check-input" type="checkbox" checked />` : `<input data-id="${todo.id}" onclick="changeStatus(event)" class="form-check-input" type="checkbox" />`}
                         </h3>
                     </div>
-                    <div class="col px-1 m-1 d-flex align-items-center">
-                        <input type="text" class="form-control form-control-lg border-0 edit-todo-input bg-transparent rounded px-3" readonly value="${todo.title}" />
+                    <div class="col px-1 m-1 d-flex align-items-center position-relative">
+                        <input onclick="editTodo(event)" type="text" class="form-control form-control-lg border-0 edit-todo-input bg-transparent rounded px-3" readonly value="${todo.title}" />
+                        <i onclick="confirmEditTodo(event)" data-id='${todo.id}' class="bi bi-check-circle text-warning btn me-3 p-0 fs-4 position-absolute end-0 d-none"></i>
                     </div>
                     <div class="col-auto m-1 p-0 px-3 d-none">
                     </div>
@@ -68,7 +68,6 @@ let $ = document;
                         <div class="row d-flex align-items-center justify-content-end">
                             <h5 class="m-0 p-0 px-2">
                                 <i data-id='${todo.id}' onclick="removeTodo(event)" class="bi bi-trash3-fill text-danger btn m-0 p-0 fs-4" data-toggle="tooltip" data-placement="bottom" title="Delete todo"></i>
-                                <i data-id='${todo.id}' onclick="editTodo(event)" class="bi bi-check-circle text-danger btn m-0 p-0 fs-4 d-none" data-toggle="tooltip" data-placement="bottom" title="Delete todo"></i>
                             </h5>
                         </div>
                     </div>
@@ -76,8 +75,6 @@ let $ = document;
             `);
         });
     }
-
-
     function removeTodo(event){
         localStorageTodos = JSON.parse(localStorage.getItem('m-todosData'));
         todoId = event.target.dataset.id
@@ -107,9 +104,33 @@ let $ = document;
     syncToStorage(todosArray)
     showTodos(todosArray)
     }
+    function editTodo(event) {
+        event.target.removeAttribute("readonly")
+        event.target.className = 'form-control form-control-lg border-0 edit-todo-input rounded px-3'
+        event.target.nextElementSibling.className = "bi bi-check-circle text-warning btn me-3 p-0 fs-4 position-absolute end-0"
+    }
+    function confirmEditTodo(event){
+        localStorageTodos = JSON.parse(localStorage.getItem('m-todosData'));
+        todoId = event.target.dataset.id
+        todosArray = localStorageTodos;
+        let newValue = event.target.previousElementSibling.value
+        if(newValue.trim()){
+            todosArray.forEach(function (todo) {
+                if (todo.id === todoId) {
+                    todo.title = newValue 
+                }
+            })
+        }
 
+        syncToStorage(todosArray)
+        showTodos(todosArray)
 
+        console.log(event.target.className);
+        event.target.previousElementSibling.className = 'form-control form-control-lg border-0 edit-todo-input rounded px-3 bg-transparent'
+        event.targrt.className = "bi bi-check-circle text-warning btn me-3 p-0 fs-4 position-absolute end-0 d-none"
+        console.log(event.target.className);
 
+    }
 
     window.addEventListener('load', getTodos)
     addTodoBtnElem.addEventListener('click', getNewTodoInfo)
